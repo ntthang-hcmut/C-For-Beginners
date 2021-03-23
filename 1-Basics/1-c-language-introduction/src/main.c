331696fd9dae1a7766b42b5098cbbe80de61f744
ntthang-hcmut <72129386+ntthang-hcmut@users.noreply.github.com>
2021-03-23 11:42:27 +0700

    C Languge Introduction source code

---
+++ 1-Basics/1-c-language-introduction/src/main.c
@@ -0,0 +1,11 @@
+/* C Languge Introduction source code */
+// include a header file in C
+#include <stdio.h> 
+// Declare main method
+int main(void) 
+{ 
+  // Body
+	printf("C-For-Beginners"); 
+	// Return
+  return 0; 
+} 
