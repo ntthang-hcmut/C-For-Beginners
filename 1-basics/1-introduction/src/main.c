b0f05c92758d741170f9ea3de9676a0549c38516
ntthang-hcmut <thang.nguyenmusic_mta@hcmut.edu.vn>
2021-03-25 22:15:56 +0700

    C Language Introdution source code
    C Language Introdution source code

---
+++ 1-basics/1-introduction/src/main.c
@@ -0,0 +1,13 @@
+// C Languge Introduction source code
+// include a header file in C
+#include <stdio.h> 
+
+// Declare main method
+int main(void) 
+{ 
+  // Body
+	printf("C-For-Beginners");
+   
+	// Return
+  return 0; 
+} 
